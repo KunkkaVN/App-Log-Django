5cb55b19b6252cc90fb2f3edd91205e7cebd9136
TanThien <tanthienqn@gmail.com>
2019-06-10 21:01:14 +0700

    10-6

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import argparse
-import realtime_log
+from DjangoLogAnalysis import login_analysis,level_analysis,realtime_log
 parser = argparse.ArgumentParser(description='APP log')
 parser.add_argument('--m', dest='mode', help='choose mode run app!')
+parser.add_argument('--p', dest='path', help='choose mode run app!')
 args = parser.parse_args()
 
-if args.mode == 'realtime':
-	realtime_log.main()
+if args.mode == 'login':
+    path = args.path
+    #login_analysis.main(path)
+    #level_analysis.main(path)
+    realtime_log.main(path)
